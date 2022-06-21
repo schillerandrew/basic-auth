1b84c69a9a801bc62744663e7185624e259d1d9b
schillerandrew <schiller.andrew@gmail.com>
2022-06-20 22:37:52 -0700

    split app.js starter code into index.js and server.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 
 // make sure our tables are created, start up the HTTP server.
 
-let { start, sequelize } = require('/src/server.js');
+let { start, sequelize } = require('./src/server.js');
 
 sequelize.sync()
   .then(() => {
-    app.listen(3000, () => console.log('server up'));
+    app.listen(PORT, () => console.log('server up'));
   }).catch(e => {
     console.error('Could not start server', e.message);
   });
